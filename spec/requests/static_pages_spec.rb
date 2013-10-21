7cf45cb0159aa0b5ed61bab93ae7f87ba620fac8
Dave Gibbs <wavydavygibbs@googlemail.com>
2013-10-21 21:49:15 +0100

    refresh commit

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,15 @@
 require 'spec_helper'
-#require 'capybara/rails'
 describe "Static pages" do
 describe "Home page" do
 it "should have the content 'Sample App'" do
 visit '/static_pages/home'
 page.should have_content('Sample App')
-config.include Capybara::DSL
+end
+end
+describe "Help page" do
+it "should have the content 'Help'" do
+visit '/static_pages/help'
+page.should have_content('Help')
 end
 end
 end
